36b309d16fc5338494aae05eb8ed957e510b754b
Nyakingi <achiengodhiambo.j@gmail.com>
2022-10-25 14:44:33 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,14 @@
  */
 void puts2(char *str)
 {
-	int i = 0
-		for (; str[i] != '\0'; i++)
-		{
-			if ((i % 2) == 0)
-				_putchar(str[i]);
-			else
-				continue;
-		}
+	int i = 0;
+	
+	for (; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
 	_putchar('\n');
 }
